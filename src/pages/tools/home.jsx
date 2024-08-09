9ac9a5251c776cf5520865df5ab120f837806730
bubu_diep <hlitdvp98@gmail.com>
2024-08-09 18:36:14 +0700

    fix auto

--- src/pages/tools/home.jsx
+++ src/pages/tools/home.jsx
@@ -1,4 +1,4 @@
-import { Suspense, React, useEffect } from "react";
+import { Suspense, React, useEffect, useState } from "react";
 import Background from "../img/iw_bg_2.png";
 import Iwork_logo from "../img/logo_txt.png";
 import MoneyCard from "../components/money-cart";
@@ -11,22 +11,36 @@ import * as icon from "@fortawesome/free-solid-svg-icons";
 var sectionStyle = {
   backgroundImage: `url(${Background})`,
 };
+
 const Home = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
   useEffect(() => {
     const handleScroll = () => {
       const homeBody = document.querySelector('.home-body');
       const topContainer = homeBody?.querySelector('.top-container');
 
       if (homeBody && topContainer) {
+        const currentScrollPos = homeBody.scrollTop;
+        const isScrollingDown = currentScrollPos > prevScrollPos;
+
         // Calculate new height based on scroll position
-        const newHeight = Math.max(120, 250 - homeBody.scrollTop); // Adjust 200 to your original height
+        const newHeight = Math.max(120, 250 - currentScrollPos);
 
         // Apply the new height to topContainer
         topContainer.style.height = `${newHeight}px`;
 
-        // Optionally, you can add or remove classes for additional styling
-        if (homeBody.scrollTop > 0 && homeBody.scrollTop <120) {
+        // Check scroll direction
+        if (isScrollingDown) {
+          console.log('Scrolling down');
+          // Perform actions when scrolling down
+        } else {
+          console.log('Scrolling up');
+          // Perform actions when scrolling up
         }
+
+        // Update previous scroll position
+        setPrevScrollPos(currentScrollPos);
       }
     };
 
@@ -41,7 +55,8 @@ const Home = () => {
         homeBody.removeEventListener('scroll', handleScroll);
       }
     };
-  }, []);
+  }, [prevScrollPos]);
+
   return (
     <Suspense>
       <div className="body-container">
@@ -59,8 +74,8 @@ const Home = () => {
             <MoneyCard />
           </div>
         </div>
-        <div className="body-main ">
-          <div className="pd0x10 ">
+        <div className="body-main">
+          <div className="pd0x10">
             <div className="message-container">
               <div className="message">Hôm nay bạn có đi làm không?</div>
               <div className="options">
@@ -79,21 +94,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          {/* <div className="h3">Hôm nay bạn có đi làm không?</div>
-          <div className="h-items">
-            <div className="fast-check">
-              <div className="work-option">
-                <div className="work-check">
-                  <div className="logo">
-                    <FontAwesomeIcon icon={icon.faCalendarCheck} />
-                  </div>
-                  <div className="text">Chấm công ngay!</div>
-                </div>
-                <div className="work-pass">Hôm nay nghỉ</div>
-              </div>
-            </div>
-          </div> */}
-          <div className="h3 top10 ">Bảng công</div>
+          <div className="h3 top10">Bảng công</div>
           <div className="h-items pd0x10">
             <BangCong />
           </div>
@@ -104,98 +105,7 @@ const Home = () => {
                 <div className="logo"></div>
                 <div className="name">Bảng lương</div>
               </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
-              <div className="items">
-                <div className="logo"></div>
-                <div className="name">Bảng lương</div>
-              </div>
+              {/* Add more items here */}
             </div>
           </div>
         </div>
@@ -203,4 +113,5 @@ const Home = () => {
     </Suspense>
   );
 };
+
 export default Home;
