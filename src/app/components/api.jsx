ced88b9eb88b5e78c440321f9321822f4daebbed
Bubudiep <hlitdvp98@gmail.com>
2024-08-15 23:52:29 +0700

    use https ipays

--- src/app/components/api.jsx
+++ src/app/components/api.jsx
@@ -1,23 +1,27 @@
 // src/components/ApiClient.js
-const ApiClient = (host='localhost', port=5000) => {
-  const baseUrl = `http://${host}:${port}`;
+const ApiClient = (host = "ipays.vn", port = 5000) => {
+  const baseUrl = `https://${host}:${port}`;
 
   return {
     get: async (endpoint) => {
-      const response = await fetch(`${baseUrl}${endpoint}`);
+      const response = await fetch(`${baseUrl}${endpoint}`, {
+        method: "GET",
+        referrerPolicy: "no-referrer", // Chỉ định chính sách không gửi trường Referer
+      });
       if (!response.ok) {
         throw new Error(`GET request failed with status ${response.status}`);
       }
       return response.json();
     },
-    gets: async (endpoint,token) => {
+    gets: async (endpoint, token) => {
       if (!token) return "Token is required";
-      const response = await fetch(`${baseUrl}${endpoint}`,{
+      const response = await fetch(`${baseUrl}${endpoint}`, {
         method: "GET",
+        referrerPolicy: "no-referrer", // Chỉ định chính sách không gửi trường Referer
         headers: {
           "Content-Type": "application/json",
-          "Authorization": "Bearer "+token
-        }
+          Authorization: "Bearer " + token,
+        },
       });
       if (!response.ok) {
         throw new Error(`GET request failed with status ${response.status}`);
@@ -30,6 +34,7 @@ const ApiClient = (host='localhost', port=5000) => {
         headers: {
           "Content-Type": "application/json",
         },
+        referrerPolicy: "no-referrer", // Chỉ định chính sách không gửi trường Referer
         body: JSON.stringify(body),
       });
       if (!response.ok) {
@@ -40,6 +45,7 @@ const ApiClient = (host='localhost', port=5000) => {
     patch: async (endpoint, body) => {
       const response = await fetch(`${baseUrl}${endpoint}`, {
         method: "PATCH",
+        referrerPolicy: "no-referrer", // Chỉ định chính sách không gửi trường Referer
         headers: {
           "Content-Type": "application/json",
         },
@@ -52,6 +58,7 @@ const ApiClient = (host='localhost', port=5000) => {
     },
     delete: async (endpoint) => {
       const response = await fetch(`${baseUrl}${endpoint}`, {
+        referrerPolicy: "no-referrer", // Chỉ định chính sách không gửi trường Referer
         method: "DELETE",
       });
       if (!response.ok) {
